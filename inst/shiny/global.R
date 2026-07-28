005c55c923217f46b84c66ebe4d9d71a55579ae1
PaulESantos <paulefrens@gmail.com>
2026-07-28 09:01:45 -0500

    shiny update

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -9,15 +9,35 @@ suppressPackageStartupMessages({
   library(rlang)
 })
 
-# Cargar el paquete floraveg si está instalado, o los archivos de código R locales
+# Cargar el paquete floraveg si está instalado y exponer módulos internos.
+#
+# shinyapps.io instala el paquete como dependencia, pero `library(floraveg)` solo
+# adjunta las funciones exportadas. Los módulos Shiny no exportados siguen en el
+# namespace del paquete, por eso se copian al ambiente de la app.
 if (requireNamespace("floraveg", quietly = TRUE)) {
   suppressPackageStartupMessages(library(floraveg))
+
+  floraveg_ns <- asNamespace("floraveg")
+  internal_patterns <- c(
+    "^mod_",
+    "^fv_",
+    "^standardize_inventory$",
+    "^validate_inventario$"
+  )
+  internal_names <- unique(unlist(lapply(
+    internal_patterns,
+    function(pattern) ls(floraveg_ns, pattern = pattern)
+  )))
+
+  for (name in internal_names) {
+    assign(name, get(name, envir = floraveg_ns), envir = environment())
+  }
 } else {
-  # Carga de respaldo para despliegues independientes en Shiny Server / shinyapps.io
+  # Carga de respaldo para despliegues independientes en Shiny Server / shinyapps.io.
   r_paths <- c("R", "../../R", "../R")
   r_dir <- r_paths[dir.exists(r_paths)][1]
   if (!is.na(r_dir)) {
     r_files <- list.files(r_dir, pattern = "\\.[RR]$", full.names = TRUE)
-    lapply(r_files, source)
+    lapply(r_files, source, local = environment())
   }
 }
